e5869d594aa19c61a2184a50fc44a87d711adbbe
EunOh <bdohhhhh@gmail.com>
2025-05-05 02:18:36 +0900

    [update]: increase attempts

--- src/constants/iffy.const.ts
+++ src/constants/iffy.const.ts
@@ -1,5 +1,5 @@
 export const QUERY_KEY_IFFY = "iffy";
 export const QUERY_KEY_IFFY_STATUS = "iffyStatus";
 export const QUERY_KEY_ALL_GIFTS = "allGifts";
-export const MAX_POLL_ATTEMPTS = 25;
+export const MAX_POLL_ATTEMPTS = 32;
 export const POLLING_INTERVAL_MS = 5000;
